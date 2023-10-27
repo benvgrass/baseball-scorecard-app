f7626c97ce85121660d99b9b3434e0879438b1ef
Ben Grass <bvgrass@outlook.com>
2023-10-26 20:28:38 -0400

    position selector looks pretty

--- src/components/ItemSelection.tsx
+++ src/components/ItemSelection.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, {ReactNode, useState} from 'react';
-import {Listbox} from "@headlessui/react";
+import React, {Fragment, ReactNode, useState} from 'react';
+import {Listbox, Transition} from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
 interface Player {
@@ -16,6 +16,10 @@ export default function BaseballLineup() {
     const [lineup, setLineup] = useState<Player[]>(battingOrderNumbers.map((battingOrder) => ({ battingOrder, name: '', position: '' })));
     const [availablePositions, setAvailablePositions] = useState<Record<string, boolean>>(initialPositions);
     const [pitcherHitting, setPitcherHitting] = useState<boolean>(false);
+
+    function classNames(...classes: string[]): string {
+        return classes.filter(Boolean).join(' ')
+    }
 
     const handleNameChange = (battingOrder: number, playerName: string) => {
         const updatedLineup = lineup.map((player) => {
@@ -122,19 +126,57 @@ export default function BaseballLineup() {
                                 >
                                     {({ open }) => (
                                         <>
-                                            <Listbox.Button>{player.position || '--'}</Listbox.Button>
-                                            <Listbox.Options>
-                                                {positions.reduce((acc, position) => {
-                                                        if (availablePositions[position]) {
-                                                            acc.push(
-                                                                <Listbox.Option key={position} value={position}>
-                                                                    {position}
-                                                                </Listbox.Option>);
+                                            <div className="relative mt-2">
+                                                <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                    <span className="block truncate">{player.position || '--'}</span>
+                                                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                                        <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                    </span>
+                                                </Listbox.Button>
+                                                <Transition
+                                                    show={open}
+                                                    as={Fragment}
+                                                    leave="transition ease-in duration-100"
+                                                    leaveFrom="opacity-100"
+                                                    leaveTo="opacity-0"
+                                                >
+                                                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                                        {positions.reduce((acc, position) => {
+                                                                if (availablePositions[position]) {
+                                                                    acc.push(
+                                                                        <Listbox.Option key={position}
+                                                                                        className={({ active }) =>
+                                                                                            classNames(
+                                                                                                active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                                                                                'relative cursor-default select-none py-2 pl-3 pr-9'
+                                                                                            )
+                                                                                        }
+                                                                                        value={position}>
+                                                                            {({ selected, active }) => (
+                                                                                <>
+                                                                                    <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                                                                        {position}
+                                                                                    </span>
+                                                                                    {selected ? (
+                                                                                        <span
+                                                                                            className={classNames(
+                                                                                                active ? 'text-white' : 'text-indigo-600',
+                                                                                                'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                                                            )}
+                                                                                        >
+                                                                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                                        </span>) : null}
+                                                                                </>
+                                                                            )}
+
+                                                                        </Listbox.Option>);
+                                                                }
+                                                                return acc;
+                                                            }, new Array<ReactNode>())
                                                         }
-                                                        return acc;
-                                                    }, new Array<ReactNode>())
-                                                }
-                                            </Listbox.Options>
+                                                    </Listbox.Options>
+                                                </Transition>
+                                            </div>
                                         </>
                                     )}
                                 </Listbox>

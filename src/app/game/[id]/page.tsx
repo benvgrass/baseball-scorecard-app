61facbab75d172e2f27cdc14e7bf9849764b5a1b
Ben Grass <bvgrass@outlook.com>
2023-11-07 17:15:53 -0500

    display fetched game data in game/[id] page

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,8 +1,40 @@
+import { getGameData } from "@/utils/newGameActions"
+import {Player} from "@/app/game/page";
 
-export default function ScoreGame( {params}: {params: {id: string}}) {
+export default async function ScoreGame( {params}: {params: {id: string}}) {
+    const data = await getGameData(params.id);
+
+    const date: Date = new Date(data.date); // TODO: parse and format date
+    console.log(data);
     return (
         <>
-            <h1>Game ID {params.id}</h1>
+            <h1>id: {data._id.$oid}: {data.away.name} at {data.home.name}</h1>
+            <h2>{date.toString()}</h2>
+            <div className="grid grid-cols-2">
+                <div>
+                    <h2>Away: {data.away.name}</h2>
+                    <ul>
+                    {data.away.lineup.batters.map((player: Player, i: number) => (
+                        <li key={`away${i}`}>
+                            <h3>{player.name}</h3>
+                            <p>{player.position}</p>
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+                <div>
+                    <h2>Home: {data.home.name}</h2>
+                    <ul>
+                    {data.home.lineup.batters.map((player: Player, i: number) => (
+                        <li key={`home${i}`}>
+                            <h3>{player.name}</h3>
+                            <p>{player.position}</p>
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+            </div>
+
         </>
     );
 
--- src/utils/newGameActions.ts
+++ src/utils/newGameActions.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import {Player} from "@/app/game/page";
-import { redirect } from "next/navigation";
+import {redirect} from "next/navigation";
 
 type Game = {
     away: Team;
@@ -22,26 +22,28 @@ type CreateGameResponse = {
 }
 
 export async function createNewGame(gameData: Game) {
-    try {
-        const response = await fetch("http://127.0.0.1:8000/game", {
-           method: "POST",
-           headers: {
-               "Content-Type": "application/json",
-           },
-            body: JSON.stringify(gameData),
-        });
-        console.log(`Response: ${response}`);
+    const response = await fetch("http://127.0.0.1:8000/gametest", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(gameData),
+    });
+    console.log(`Response: ${response}`);
+    if (response.ok) {
         const result: CreateGameResponse = await response.json();
         console.log(`Result: ${result}`);
         const game_id = result.game_id;
         console.log(`Game ID received: ${game_id}`);
-        // TODO: Move outside try catch
         redirect(`/game/${game_id}`);
-    } catch (e) {
-        // TODO: do something on error
-        console.log(`Error: ${e}`);
-        // alert(`Error: ${e}`);
+    } else {
+        // TODO: handle error
     }
     console.log(gameData);
-    
 }
+
+export async function getGameData(game_id: string) {
+    const response = await fetch(`http://127.0.0.1:8000/game/${game_id}`);
+    return await response.json();
+}
+
